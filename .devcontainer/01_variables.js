451318bc96e9d48ccfec1dcc84d9e1868ef87f1a
Karan8768 <108995285+Karan8768@users.noreply.github.com>
2024-07-12 13:28:44 +0000

    variable and datatypes

---
+++ .devcontainer/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+//accountId=2
+accountEmail = "karan@gamil.com"
+accountPassword = "212121"
+accountCity = "Delhi"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+//console.log(accountId);
---
+++ .devcontainer/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";  //treat all Js code in newer version 
+
+//alert(3 +3)  we are using node js, not browser pop up will not come
+let name="hitesh";
+let age = 18;
+let isLoggedIn = false;
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof "hitesh");
+//number
+//bigInt
+//string
+//boolean
+//null
+//undefined
+//symbol
+//Object
