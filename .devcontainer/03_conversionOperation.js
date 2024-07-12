b1e1e5093c1926f131176fb3166acf5ffab78959
Karan8768 <108995285+Karan8768@users.noreply.github.com>
2024-07-12 17:05:58 +0000

    updated

--- .devcontainer/03_conversionOperation.js
+++ .devcontainer/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "33"
-console.log(typeof score);
-console.log(Number(score));
+// console.log(typeof score);
+// console.log(Number(score));
 let sc = "33abc"
 let valueInNumber = Number(sc) // conversion done in number but value store will be NaN (Not a Number)
 //bcz 33abc is not a number
-console.log(valueInNumber); 
+// console.log(valueInNumber); 
 //"33" -> 33
 //"33abc" -> NaN
 //"null" -> 0 undefined -> NaN , true -> 1 
@@ -15,5 +15,20 @@ console.log(valueInNumber);
 
 let num = 33
 let strnum = String(num)
-console.log(strnum)
-console.log(typeof strnum)
+// console.log(strnum)
+// console.log(typeof strnum)
+
+//Operations
+let value = 3
+let negValue = -value
+// console.log(negValue)
+// console.log(2**3);
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+// console.log("1"+"2")
+
+console.log(null>0);
+console.log(null==0)
+console.log(null>=0);
+console.log("2">1);
+console.log("2"===2);
